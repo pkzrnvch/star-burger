1c22976142759d499e6339127385a37783b9b3fc
pkzrnvch <pkazarnovich@gmail.com>
2022-05-23 02:09:27 +0500

    Add data validation for def register_order

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -64,30 +64,49 @@ def product_list_api(request):
 
 @api_view(['POST'])
 def register_order(request):
-    try:
-        serialized_order = request.data
-    except ValueError:
-        return Response({
-            'error': 'Cannot serialize json',
-        })
-    print(serialized_order)
+    order_details = request.data
+    if 'products' not in order_details:
+        return Response(['Products field is required'], status=400)
+    if order_details['products'] is None:
+        return Response(['Products field cannot be empty'], status=400)
+    if not isinstance(order_details['products'], list):
+        return Response(['Products must be a list'], status=400)
+    if not order_details['products']:
+        return Response(['Products cannot be an empty list'], status=400)
+    all_product_ids = list(Product.objects.all().values_list('id', flat=True))
+    order_product_ids = [position['product'] for position in order_details['products']]
+    print(order_product_ids)
+    for product_id in order_product_ids:
+        if product_id not in all_product_ids:
+            return Response(['Invalid product id'], status=400)
+    if any(not i for i in order_details.values()):
+        return Response(['This field cannot be empty'], status=400)
+    keys_to_check = ['firstname', 'lastname', 'address', 'phonenumber']
+    for key in keys_to_check:
+        if key not in order_details:
+            return Response(['Missing a required field'], status=400)
+        if not isinstance(order_details[key], str):
+            return Response(['This field has to be string'], status=400)
+    print(order_details)
     parsed_phone_number = phonenumbers.parse(
-        serialized_order['phonenumber'],
+        order_details['phonenumber'],
         'RU'
     )
+    if not phonenumbers.is_valid_number(parsed_phone_number):
+        return Response(['Invalid phone number'], status=400)
     normalized_phone_number = phonenumbers.format_number(
                 parsed_phone_number,
                 phonenumbers.PhoneNumberFormat.E164
             )
     order = Order.objects.create(
-        client_name=serialized_order['firstname'],
-        client_surname=serialized_order['lastname'],
-        address=serialized_order['address'],
+        client_name=order_details['firstname'],
+        client_surname=order_details['lastname'],
+        address=order_details['address'],
         contact_phone=normalized_phone_number
     )
-    for position in serialized_order['products']:
+    for position in order_details['products']:
         order.items.create(
             product_id=position['product'],
             quantity=position['quantity']
         )
-    return Response(serialized_order)
+    return Response(order_details)
