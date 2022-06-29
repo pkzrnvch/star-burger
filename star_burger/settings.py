c1b4a7237d65a03e7db9488ef080b5e2cd239a32
pkzrnvch <pkazarnovich@gmail.com>
2022-06-30 00:11:26 +0500

    switch to PostgreSQL DB

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -98,11 +98,7 @@ WSGI_APPLICATION = 'star_burger.wsgi.application'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-    )
-}
+DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
